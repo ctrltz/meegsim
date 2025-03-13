6aeadf3a64709ee58985a4d14263aeb5b0bbb004
Nikolai Kapralov <4dvlup@gmail.com>
2025-03-13 16:13:04 +0100

    DOC: improve the example plot

--- examples/coupling.py
+++ examples/coupling.py
@@ -29,13 +29,35 @@ for i_lag, lag in enumerate(phase_lags):
             y = ppc_shifted_copy_with_noise(
                 x, sfreq, target_coh, lag, fmin, fmax, random_state=seed
             )
+            # x is leading y
             coh_sim[i_lag, i_coh, i_sim] = compute_plv(
-                x, y, n=1, m=1, plv_type="complex", coh=True
+                y, x, n=1, m=1, plv_type="complex", coh=True
             )[0, 0]
 
 target_values = np.tile(target_coherence[:, np.newaxis], (1, n_simulations))
-fig, axes = plt.subplots(ncols=n_lags)
-for ax, lag in zip(axes, phase_lags):
-    ax.scatter(target_values.flatten(), np.abs(coh_sim[i_lag, :, :].flatten()))
-    ax.plot([0, 1], [0, 1], c="gray", ls="--")
+fig, axes = plt.subplots(nrows=2, ncols=n_lags, figsize=(9, 6))
+for i_lag, lag in enumerate(phase_lags):
+    lag_in_degrees = np.rad2deg(lag)
+    obtained_coh = np.abs(coh_sim[i_lag, :, :])
+    obtained_lag = np.rad2deg(np.angle(coh_sim[i_lag, :, :]))
+    mean_coh = obtained_coh.mean(axis=1)
+    mean_lag = obtained_lag.mean(axis=1)
+
+    ax_coh = axes[0, i_lag]
+    ax_coh.scatter(target_values.flatten(), obtained_coh.flatten(), c="gray", alpha=0.1)
+    ax_coh.plot(target_coherence, mean_coh, c="black", lw=2)
+    ax_coh.set_xlim([0, 1])
+    ax_coh.set_ylim([0, 1])
+    ax_coh.set_xlabel("Target coherence")
+    ax_coh.set_ylabel("Obtained coherence")
+    ax_coh.set_title(f"phase lag = {lag_in_degrees:.0f} degrees")
+
+    ax_lag = axes[1, i_lag]
+    ax_lag.scatter(target_values.flatten(), obtained_lag.flatten(), c="gray", alpha=0.1)
+    ax_lag.plot(target_coherence, mean_lag, c="black", lw=2)
+    ax_lag.set_xlim([0, 1])
+    ax_lag.set_ylim([-180, 180])
+    ax_lag.set_xlabel("Target coherence")
+    ax_lag.set_ylabel("Obtained phase lag (degrees)")
+fig.tight_layout()
 plt.show(block=True)
