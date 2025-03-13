298a25214786c2c96e65880f39ea090fef04191f
Nikolai Kapralov <4dvlup@gmail.com>
2025-03-13 11:41:13 +0100

    WIP patch size example

--- blocks/plot_patch_size.py
+++ blocks/plot_patch_size.py
@@ -6,7 +6,15 @@ This example shows how to control the area of the patch and highlights its
 effect on the resulting leadfield of the source.
 """
 
+import mne
+
 from mne.datasets import sample
 
 data_path = sample.data_path()
-print(data_path)
+meg_path = data_path / "MEG" / "sample"
+fwd_path = meg_path / "sample_audvis-meg-eeg-oct-6-fwd.fif"
+
+fwd = mne.read_forward_solution(fwd_path)
+indices_by_type = mne.channel_indices_by_type(fwd["info"])
+eeg_ch_names = fwd.ch_names[indices_by_type["eeg"]]
+fwd_eeg = fwd.pick_channels(eeg_ch_names)
